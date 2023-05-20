796d3268271cce0c6df419451b5f0f3bba465fd0
Nguyen Trung Kien <nguyentrungkien.work01@gmail.com>
2023-05-20 22:23:11 +0700

    fix bug

--- src/main/java/edu/ou/authqueryservice/controller/permission/PermissionFindAllCurrentUserController.java
+++ src/main/java/edu/ou/authqueryservice/controller/permission/PermissionFindAllCurrentUserController.java
@@ -1,15 +1,11 @@
 package edu.ou.authqueryservice.controller.permission;
 
-import edu.ou.authqueryservice.common.constant.CodeStatus;
 import edu.ou.authqueryservice.common.constant.EndPoint;
 import edu.ou.authqueryservice.data.pojo.request.permission.PermissionFindByAccountIdRequest;
-import edu.ou.coreservice.common.constant.Message;
 import edu.ou.coreservice.common.constant.SecurityPermission;
 import edu.ou.coreservice.common.util.SecurityUtils;
 import edu.ou.coreservice.data.pojo.request.base.IBaseRequest;
 import edu.ou.coreservice.data.pojo.response.base.IBaseResponse;
-import edu.ou.coreservice.data.pojo.response.impl.SuccessPojo;
-import edu.ou.coreservice.data.pojo.response.impl.SuccessResponse;
 import edu.ou.coreservice.service.base.IBaseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -37,14 +33,8 @@ public class PermissionFindAllCurrentUserController {
     public ResponseEntity<IBaseResponse> findAllPermissionOfCurrentUser() {
         final int accountId = SecurityUtils.getCurrentAccount(rabbitTemplate).getAccountId();
         return new ResponseEntity<>(
-                new SuccessResponse<>(
-                        new SuccessPojo<>(
-                                permissionFindByAccountIdService.execute(
-                                        new PermissionFindByAccountIdRequest().setAccountId(accountId)),
-                                CodeStatus.SUCCESS,
-                                Message.Success.SUCCESSFUL
-                        )
-                ),
+                permissionFindByAccountIdService.execute(
+                        new PermissionFindByAccountIdRequest().setAccountId(accountId)),
                 HttpStatus.OK
         );
     }
