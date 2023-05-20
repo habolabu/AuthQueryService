594e7272f98ca4b72c4f249b734e89d33c00cc78
Nguyen Trung Kien <nguyentrungkien.work01@gmail.com>
2023-05-20 21:56:24 +0700

    fix bug

--- src/main/java/edu/ou/authqueryservice/controller/permission/PermissionFindAllCurrentUserController.java
+++ src/main/java/edu/ou/authqueryservice/controller/permission/PermissionFindAllCurrentUserController.java
@@ -2,12 +2,15 @@ package edu.ou.authqueryservice.controller.permission;
 
 import edu.ou.authqueryservice.common.constant.CodeStatus;
 import edu.ou.authqueryservice.common.constant.EndPoint;
+import edu.ou.authqueryservice.data.pojo.request.permission.PermissionFindByAccountIdRequest;
 import edu.ou.coreservice.common.constant.Message;
 import edu.ou.coreservice.common.constant.SecurityPermission;
 import edu.ou.coreservice.common.util.SecurityUtils;
+import edu.ou.coreservice.data.pojo.request.base.IBaseRequest;
 import edu.ou.coreservice.data.pojo.response.base.IBaseResponse;
 import edu.ou.coreservice.data.pojo.response.impl.SuccessPojo;
 import edu.ou.coreservice.data.pojo.response.impl.SuccessResponse;
+import edu.ou.coreservice.service.base.IBaseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
@@ -22,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(EndPoint.Permission.BASE)
 public class PermissionFindAllCurrentUserController {
     private final RabbitTemplate rabbitTemplate;
-
+    private final IBaseService<IBaseRequest, IBaseResponse> permissionFindByAccountIdService;
     /**
      * find all permission of current user
      *
@@ -32,10 +35,12 @@ public class PermissionFindAllCurrentUserController {
     @PreAuthorize(SecurityPermission.AUTHENTICATED)
     @GetMapping()
     public ResponseEntity<IBaseResponse> findAllPermissionOfCurrentUser() {
+        final int accountId = SecurityUtils.getCurrentAccount(rabbitTemplate).getAccountId();
         return new ResponseEntity<>(
                 new SuccessResponse<>(
                         new SuccessPojo<>(
-                                SecurityUtils.getCurrentAccount(rabbitTemplate).getPermissions(),
+                                permissionFindByAccountIdService.execute(
+                                        new PermissionFindByAccountIdRequest().setAccountId(accountId)),
                                 CodeStatus.SUCCESS,
                                 Message.Success.SUCCESSFUL
                         )
